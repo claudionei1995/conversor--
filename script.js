73ba2442ee13595936ac2be273ac1afe9005cdbb
Claudionei1995 <claudionei0580@gmail.com>
2024-03-24 14:55:44 -0300

    conversor java

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-const convertbutton = document.querySelector(".convert-button")
+const btn = document.getElementById("convert-button")
+
+function covertvaluos() {
+
+    const currencyvaluos = document.querySelector(".input-currency").value
+    const convert = document.querySelector(".value1")
+    const coverto = document.querySelector(".value2")
+    const dolartoday = 5.2
+    const conversoes = currencyvaluos / dolartoday
+
+    convert.innerHTML = new Intl.NumberFormat("en-BR", {
+        styles: "currency",
+        currency: "BRL"
+    }).format(currencyvaluos);
+
+    
+
+    coverto.innerHTML = new Intl.NumberFormat("en-US", {
+        styles: "currency",
+        currency: "USD"
+    }).format(conversoes);
 
 
 
- 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn.addEventListener("click", covertvaluos) 
