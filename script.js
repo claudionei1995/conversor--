7b3f6ae9b1ffc914f2ba05586a9d947f5d8ff551
Claudionei1995 <claudionei0580@gmail.com>
2024-03-26 17:19:11 -0300

    alteracao javascripth

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 const btn = document.getElementById("convert-button")
-
+const currencyselect = document.querySelector(".dolaramericano")
 function covertvaluos() {
 
     const currencyvaluos = document.querySelector(".input-currency").value
     const convert = document.querySelector(".value1")
     const coverto = document.querySelector(".value2")
     const dolartoday = 5.2
-    const conversoes = currencyvaluos / dolartoday
+    const eurotoday = 6.2
 
-    convert.innerHTML = new Intl.NumberFormat("en-BR", {
-        styles: "currency",
-        currency: "BRL"
-    }).format(currencyvaluos);
+    if (currencyselect.value == "dolar") {
+        coverto.innerHTML = new Intl.NumberFormat("en-US", {
+            styles: "currency",
+            currency: "USD"
+        }).format(currencyvaluos / dolartoday);
 
-    
+    }
 
-    coverto.innerHTML = new Intl.NumberFormat("en-US", {
-        styles: "currency",
-        currency: "USD"
-    }).format(conversoes);
+    if (currencyselect.value == "euro") {
+        coverto.innerHTML = new Intl.NumberFormat("de-DE", {
+
+            style: "currency",
+            currency: "EUR"
+
+        }).format(currencyvaluos / eurotoday)
+
+        
+        console.log(currencyselect.value)
+
+        convert.innerHTML = new Intl.NumberFormat("en-BR", {
+            styles: "currency",
+            currency: "BRL"
+        }).format(currencyvaluos);
 
 
+    }
 
 }
 
-btn.addEventListener("click", covertvaluos) 
+
+btn.addEventListener("click", covertvaluos)
